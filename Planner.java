38966ef190f270cdcb7acb3bcdaad3cd11b8ae77
Lorenzo Martinico <lzmartinico@gmail.com>
2016-02-11 19:17:59 +0000

    Add Planner class with rough outline of algorithm

---
+++ Planner.java
@@ -0,0 +1,10 @@
+public class Planner {
+    //0. Parse and instantiate
+    //1. Divide map into voronoi regions
+    //2. Sort areas by number of orders that need to be delivered there
+    //3. Send drones to warehouse in order (one to each or as many as necessary?)
+    //4. Each drone will load as many products as it can for houses that are close by each other
+    // (but we prefer a single drone to fill out one order)
+    //5. Deliver to the house in order of distance from warehouse
+    //6. Go to next closest warehouse with the most orders to be fulfilled in that area
+}
